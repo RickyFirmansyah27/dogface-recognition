7712f585f6603ae1e44b1627aba75411272b24b3
Ricky Firmansyah <ricky.firmansyah@dansmultipro.com>
2023-07-07 21:18:54 +0700

    changes fix

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, Dense, MaxPooling2D, Flatten
 from keras.callbacks import EarlyStopping
-from sklearn.metrics import confusion_matrix
 
 def get_files(directory):
     if not os.path.exists(directory):
@@ -20,79 +19,20 @@ def get_files(directory):
             count += len(glob.glob(os.path.join(current_path, dr + "/*")))
     return count
 
-# def validate():
-#     model = keras.models.load_model("model.h5")
-#     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-#     true_labels = []
-#     predictions = []
-#     train_dir = "dataset/split_dataset/train"
-#     val_dir = "dataset/split_dataset/validation"
-#     test_dir = "dataset/split_dataset/test"
-
-#     train_samples = get_files(train_dir)
-#     num_classes = len(glob.glob(train_dir + "/*"))
-#     val_samples = get_files(val_dir)
-#     test_samples = get_files(test_dir)
-
-#     st.write('train_samples:', train_samples)
-#     st.write('num_classes:', num_classes)
-#     st.write('val_samples:', val_samples)
-#     st.write('test_samples:', test_samples)
-
-#     true_labels.extend(['Golden'] * val_samples)
-#     true_labels.extend(['Husky'] * val_samples)
-#     predictions.extend(['Golden'] * val_samples)
-#     predictions.extend(['Husky'] * val_samples)
-
-#     st.write('Confusion Matrix:')
-#     st.write(confusion_matrix(true_labels, predictions))
 def validate():
-    model = keras.models.load_model("model.h5")
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-    true_labels = []
-    predictions = []
+    train_dir = "dataset/split_dataset/train"
     val_dir = "dataset/split_dataset/validation"
+    test_dir = "dataset/split_dataset/test"
 
+    train_samples = get_files(train_dir)
+    num_classes = len(glob.glob(train_dir + "/*"))
     val_samples = get_files(val_dir)
-    num_classes = model.output_shape[-1]
+    test_samples = get_files(test_dir)
 
-    # Iterate over the validation directory and predict labels for each image
-    for current_path, dirs, files in os.walk(val_dir):
-        for file in files:
-            image_path = os.path.join(current_path, file)
-            im = Image.open(image_path)
-            width, height = im.size
-
-            if width == height:
-                im = im.resize((150, 150), Image.ANTIALIAS)
-            else:
-                if width > height:
-                    left = width / 2 - height / 2
-                    right = width / 2 + height / 2
-                    top = 0
-                    bottom = height
-                    im = im.crop((left, top, right, bottom))
-                    im = im.resize((150, 150), Image.ANTIALIAS)
-                else:
-                    left = 0
-                    right = width
-                    top = 0
-                    bottom = width
-                    im = im.crop((left, top, right, bottom))
-                    im = im.resize((150, 150), Image.ANTIALIAS)
-
-            ar = np.asarray(im)
-            ar = ar.astype('float32')
-            ar /= 255.0
-            ar = ar.reshape(-1, 150, 150, 3)
-
-            # Perform prediction using the model
-            predictions.append(np.argmax(model.predict(ar)))
-            true_labels.append(os.path.basename(current_path))
-
-    st.write('Confusion Matrix:')
-    st.write(confusion_matrix(true_labels, predictions, labels=range(num_classes)))
-
+    st.write('train_samples:', train_samples)
+    st.write('num_classes:', num_classes)
+    st.write('val_samples:', val_samples)
+    st.write('test_samples:', test_samples)
 
 
 def process_and_predict(file):
